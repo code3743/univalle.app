716c33e6b37b1a51c34e76e5e361d07998b45518
code3743 <jota.lopez@correounivalle.edu.co>
2026-03-06 17:27:38 -0500

    Update Astro configuration and add pnpm workspace file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://code3743.github.io",
+  base: "/univalle.app/",
 });
