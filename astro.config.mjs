06957510e10b5390ae759ec6ddc3df5940699736
code3743 <jota.lopez@correounivalle.edu.co>
2026-03-07 08:31:09 -0500

    feat: update Astro configuration and add @types/node as a dev dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  //site: "https://code3743.github.io",
-  //base: "/univalle.app/",
+  site: "https://code3743.github.io",
+  base: isGitHubActions ? "/univalle.app/" : "/",
 });
